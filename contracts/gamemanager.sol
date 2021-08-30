bd0c1eef1d5fae494db88dfdda4316edb42cdd1e
Theo Breakspear <theo.breakspear@gmail.com>
2021-08-30 12:06:33 +0100

    fixed comment typo

--- contracts/gamemanager.sol
+++ contracts/gamemanager.sol
@@ -4,7 +4,8 @@ pragma solidity >=0.4.16 <0.9.0;
 contract GameManager {
 
     event NewGame(uint gameId, address player1);
-    event joinedGame(uint gameId)
+    event JoinedGame(uint gameId);
+    event NewWager(uint amount);
 
     //represents a game being played
     struct Game {
@@ -17,7 +18,7 @@ contract GameManager {
     //maps game ID to game & stores active games
     mapping (uint => Game) public activeGames; 
     
-    //takes values for proposed player2 and wager to create new game with msg.sender as player1  
+    //takes values for proposed player2 to create new game with msg.sender as player1  
     function newGame(address _player2) external {
         uint _gameId = uint(keccak256(abi.encodePacked(msg.sender, _player2)));
         activeGames[_gameId] = Game(msg.sender, _player2, 0, false);
@@ -37,16 +38,17 @@ contract GameManager {
     //player 2 calls to confirm game
     function joinGame(uint _gameId) external onlyPlayer2(_gameId) {
         activeGames[_gameId].bothReady = true;
-        emit joinedGame(_gameId, _player2);
+        emit JoinedGame(_gameId, _player2);
     }
 
     //player 1 calls to set wager
-    function newWager(uint _amount, uint _gameId) payable onlyPlayer1(_gameId) {
+    function newWager(uint _amount, uint _gameId) private payable onlyPlayer1(_gameId) {
         activeGames[_gameId].wager = _amount;
+        emit NewWager(_amount);
     }
 
     //player2 calls to join wager
-    function joinWager(uint _gameId) payable onlyPlayer2(_gameId) {
+    function joinWager(uint _gameId) private payable onlyPlayer2(_gameId) {
         _amount = activeGames[_gameId].wager;
         
     }
